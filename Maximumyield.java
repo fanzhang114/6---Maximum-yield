2faadb4c5da6412f471edc2a68ca85cffd36a829
fanzhang114 <fanzhang114@gmail.com>
2015-09-23 15:36:15 -0700

    Update Maximumyield.java

--- Maximumyield.java
+++ Maximumyield.java
@@ -7,7 +7,7 @@ public class Maximumyield {
         	for(int i=1; i<=strip.length;i++){
         		//matrix[i][0] is the max yield of 0 to i-1 square
         		matrix[i][0]=Math.max(matrix[i-1][0], matrix[i-1][1]);
-          		//matrix[i][1] is the yield besed on matrix[i-1][0] when we select the i-1 square to mine
+          		//matrix[i][1] is the yield besed on matrix[i-1][0] when we select the ith square to mine
             		matrix[i][1]=strip[i-1]+matrix[i-1][0];
         	}
         	return Math.max(matrix[strip.length][1], matrix[strip.length][0]);
